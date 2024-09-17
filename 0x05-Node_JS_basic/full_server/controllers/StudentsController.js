e1de12c1315fadd3e4ba8a02925112b89c411e35
MarkEddy001 <markeddy254@gmail.com>
2024-09-17 09:17:14 +0000

    Update

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,25 +2,14 @@ import readDatabase from '../utils';
 
 const VALID_MAJORS = ['CS', 'SWE'];
 
-
 class StudentsController {
   static getAllStudents(request, response) {
-    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
+    const dataPath = process.env.DATABASE_PATH || '';
 
     readDatabase(dataPath)
       .then((studentGroups) => {
         const responseParts = ['This is the list of our students'];
-        // A comparison function for ordering a list of strings in ascending
-        // order by alphabetic order and case insensitive
-        const cmpFxn = (a, b) => {
-          if (a[0].toLowerCase() < b[0].toLowerCase()) {
-            return -1;
-          }
-          if (a[0].toLowerCase() > b[0].toLowerCase()) {
-            return 1;
-          }
-          return 0;
-        };
+        const cmpFxn = (a, b) => a[0].toLowerCase().localeCompare(b[0].toLowerCase());
 
         for (const [field, group] of Object.entries(studentGroups).sort(cmpFxn)) {
           responseParts.push([
@@ -32,37 +21,34 @@ class StudentsController {
         response.status(200).send(responseParts.join('\n'));
       })
       .catch((err) => {
-        response
-          .status(500)
-          .send(err instanceof Error ? err.message : err.toString());
+        response.status(500).send(err instanceof Error ? err.message : err.toString());
       });
   }
 
   static getAllStudentsByMajor(request, response) {
-    const dataPath = process.argv.length > 2 ? process.argv[2] : '';
+    const dataPath = process.env.DATABASE_PATH || '';
     const { major } = request.params;
 
     if (!VALID_MAJORS.includes(major)) {
-      response.status(500).send('Major parameter must be CS or SWE');
+      response.status(400).send('Major parameter must be CS or SWE');
       return;
     }
+
     readDatabase(dataPath)
       .then((studentGroups) => {
-        let responseText = '';
-
-        if (Object.keys(studentGroups).includes(major)) {
-          const group = studentGroups[major];
-          responseText = `List: ${group.map((student) => student.firstname).join(', ')}`;
+        if (!studentGroups[major]) {
+          response.status(404).send('No students found for the given major');
+          return;
         }
+
+        const group = studentGroups[major];
+        const responseText = `List: ${group.map((student) => student.firstname).join(', ')}`;
         response.status(200).send(responseText);
       })
       .catch((err) => {
-        response
-          .status(500)
-          .send(err instanceof Error ? err.message : err.toString());
+        response.status(500).send(err instanceof Error ? err.message : err.toString());
       });
   }
 }
 
 export default StudentsController;
-module.exports = StudentsController;
