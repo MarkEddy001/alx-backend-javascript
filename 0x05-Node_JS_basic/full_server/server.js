e89f2ad377cfb9a1bff7d0b677e46c01f86ef46a
MarkEddy001 <markeddy254@gmail.com>
2024-09-17 08:42:32 +0000

    Update

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import mapRoutes from './routes';
 
 const app = express();
-const PORT = 1245;
+const PORT = process.env.PORT || 1245;
 
 mapRoutes(app);
-app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+  });
+}
 
 export default app;
-module.exports = app;
